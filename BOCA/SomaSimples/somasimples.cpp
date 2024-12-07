cefa6d59dc6018b897c2602a63ec2a9e883e8522
FilippiMnz <imperilkill12@gmail.com>
2024-12-07 12:22:52 -0300

    Boca

---
+++ BOCA/SomaSimples/somasimples.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+    int A, B, X;
+    cin >> A;
+    cin >> B;   
+    X = A + B;
+    cout << "X = " << X << endl;
+
+    return 0;
+
+}
